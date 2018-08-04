2fc5615c327a3e284b5f86ce1c4be3de568d79ae
Kanji Takahashi <ka.takahashi@sansan.com>
2018-08-04 16:37:40 +0900

    類似問題を取り出す計算を追加

--- app.py
+++ app.py
@@ -1,42 +1,15 @@
-import csv
 import random
 
 from flask import Flask, render_template, jsonify, request
+
+from utils import read_question, get_vector, get_similar_question_idx
 
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
-with open("./個人情報保護士問題と解説.tsv", "r", encoding="utf-8") as fin:
-    reader = csv.DictReader(fin, delimiter="\t")
-    questions = []
-    for row in reader:
-        question = {
-            "question": row["問題"],
-            "candidates": [
-                {
-                    "text": row["選択肢1"],
-                    "correct": row["答え"] == "1",
-                    "description": row["解説1"],
-                },
-                {
-                    "text": row["選択肢2"],
-                    "correct": row["答え"] == "2",
-                    "description": row["解説2"],
-                },
-                {
-                    "text": row["選択肢3"],
-                    "correct": row["答え"] == "3",
-                    "description": row["解説3"],
-                },
-                {
-                    "text": row["選択肢4"],
-                    "correct": row["答え"] == "4",
-                    "description": row["解説4"],
-                },
-            ]
-        }
-        questions.append(question)
+questions = read_question()
+vec = get_vector(questions)
 
 
 @app.route("/")
@@ -46,9 +19,14 @@ def index():
 
 @app.route("/api/question")
 def question():
-    question = random.choice(questions)
-    random.shuffle(question["candidates"])
+    similar_question_index = request.args.get("sim_q_idx", "")
 
+    if similar_question_index == "":
+        question = random.choice(questions)
+    else:
+        question = get_similar_question_idx(vec, similar_question_index)
+
+    random.shuffle(question["candidates"])
     return jsonify(question), 200
 
 
